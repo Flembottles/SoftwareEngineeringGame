2bd7ffb4a0fc231daea9c9b09c3e798003ab2b45
Daniel <dan.petru@hotmail.com>
2014-04-13 19:36:57 -0400

    Added SFML Title Screen & Help Screen

--- Spectrum/Spectrum/main.cpp
+++ Spectrum/Spectrum/main.cpp
@@ -9,7 +9,8 @@
 Player player;
 Platform platform(Platform::RED);
 bool colorKeyPressed = false;
-
+float moveX1 = 340;
+float moveY1 = 300;
 void setup()
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -131,19 +132,93 @@ void render()
 
 int main(int argc, char **argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(800, 600);
-	glutInitWindowPosition(100, 50);
-	glutCreateWindow("Spectrum");
+sf::Texture tscreen;
+sf::Texture ptexture;
+sf::Texture htexture;
+if(!tscreen.loadFromFile("titlescreen.png"))
+   {
+			return 0;
+	}
+if (!ptexture.loadFromFile("tpointer.png"))
+		{
+			return 0;
+		}
+if (!htexture.loadFromFile("help.png"))
+		{
+			return 0;
+		}
+sf::Sprite tpointer(ptexture);
+sf::Sprite titlescreen(tscreen);
+sf::Sprite helps(htexture);
+sf::RenderWindow window1(sf::VideoMode(900,600), "Title Screen"); // Title Screen Window
+while (window1.isOpen())
+    {
+	
+		tpointer.setPosition(moveX1,moveY1);
+		sf::Event event;
+		while (window1.pollEvent(event))
+        {
+				
+		
+			if (event.key.code == sf::Keyboard::Space && (moveY1 == 300))
+			{
+				window1.close();
+				glutInit(&argc, argv);
+				glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+				glutInitWindowSize(800, 600);
+				glutInitWindowPosition(100, 50);
+				glutCreateWindow("Spectrum");
 
-	setup();
-	glutKeyboardFunc(keyDown);
-	glutKeyboardUpFunc(keyUp);
-	glutSpecialFunc(specialKeyDown);
-	glutSpecialUpFunc(specialKeyUp);
-	glutTimerFunc(16,timer,16);
-	glutReshapeFunc(resize);
-	glutDisplayFunc(render);
-	glutMainLoop();
+				setup();
+				glutKeyboardFunc(keyDown);
+				glutKeyboardUpFunc(keyUp);
+				glutSpecialFunc(specialKeyDown);
+				glutSpecialUpFunc(specialKeyUp);
+				glutTimerFunc(16,timer,16);
+				glutReshapeFunc(resize);
+				glutDisplayFunc(render);
+				glutMainLoop();
+			}
+			if (event.key.code == sf::Keyboard::Space && (moveY1 == 386))
+		    {
+				 window1.close();
+			}
+			if (event.type == sf::Event::KeyPressed) 
+			{
+				 if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && (moveY1 > 300))
+				{
+					moveY1 -= 43;
+				}
+				  if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && (moveY1 < 386 ))
+				{
+					moveY1 += 43;
+				}
+				  if (event.key.code == sf::Keyboard::Space && (moveY1 == 343))
+				 {
+				  sf::RenderWindow instruct(sf::VideoMode(400, 400), "Instructions");
+				   while (instruct.isOpen())
+					 {
+						// sf::Event::GainedFocus;
+						 while (instruct.pollEvent(event))
+						 {
+							
+							  // "close requested" event: we close the window
+							 if (event.key.code == sf::Keyboard::B)
+								 instruct.close();
+
+						 }
+						instruct.clear(sf::Color::Black);
+						instruct.draw(helps);
+						instruct.display();
+					  }
+				
+				  }
+			}
+
+		}
+		window1.clear(sf::Color::Black);
+		window1.draw(titlescreen);
+		window1.draw(tpointer);
+		window1.display();
+		}
 }
